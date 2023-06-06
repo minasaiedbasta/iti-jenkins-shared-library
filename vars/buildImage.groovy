743f2c1cebc5b7d7001c246bb28a39d6f1a3d24e
minasaiedbasta <mina.saied.basta@gmail.com>
2023-06-06 15:36:00 +0300

    use imageName variable

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(string $imageName) {
     echo "building docker image..."
     withCredentials ([usernamePassword(credentialsId:'dockerhub',usernameVariable: 'USER',passwordVariable:'PASS')]) {
-        sh 'docker build -t minasaiedbasta/demo-maven-app:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push minasaiedbasta/demo-maven-app:jma-2.0'
+        sh "docker push $imageName"
     }
 }
